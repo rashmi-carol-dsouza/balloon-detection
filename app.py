1b4ec821f1ef4bcd4274e1f5cffbf8ceb331e34d
rashmi-carol-dsouza <hello@rashmidsouza.com>
2022-12-29 18:45:25 +0100

    Adding upload option and saving option

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
-from flask import Flask,render_template
-# import requests
-# import json
-# import pandas as pd
-# import sys
-# from model import geoloc
+from flask import Flask,render_template, send_from_directory,url_for
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField,FileAllowed,FileRequired
+from wtforms import SubmitField
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "asddfgh"
+app.config["UPLOADED_PHOTOS_DEST"] = "uploads"
+
+photos = UploadSet("photos", IMAGES)
+configure_uploads(app,photos)
+
+class UploadForm(FlaskForm):
+    photo = FileField(
+        validators = [
+            FileAllowed(photos, " Only images are allowed"),
+            FileRequired("File Field should not be empty")
+
+        ] 
+    )
+    submit = SubmitField("Upload")
+
+
+@app.route("/uploads/<filename>")
+def get_file(filename):
+    return send_from_directory(app.config["UPLOADED_PHOTOS_DEST"],filename) 
 
 
 @app.route("/",methods=["GET","POST"])
 def home():
-    return render_template("index.html")
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = url_for("get_file",filename=filename)
+    else:
+        file_url=None
+    return render_template("index.html",form=form,file_url=file_url)
 
 
-# @app.route("/toilets",methods=["GET"])
-# def toilet():
-#     args = request.args
-#     lat1 = args.get('lat')
-#     lng1 = args.get('lng')
-#     timing = args.get('open24')
-#     cost = args.get('free')
-#     if_accessible = args.get('accessible')
-#     result = geoloc(lat1,lng1,timing,cost,if_accessible)
-#     return result
-        
+       
 
 if __name__ == "__main__":
     app.run(debug=True)
